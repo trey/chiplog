5dda8ff7704b0dfd6eb10407e8528f3f5dd144e2
Trey Piepmeier <trey@sitening.com>
2008-07-13 13:36:04 -0500

    Starting to customize admin based on my django-admin-style project.

---
+++ static/admin/css/img/README.markdown
@@ -0,0 +1,3 @@
+Use this folder for background images in the Django admin.
+
+[More site structuring ideas.](http://github.com/trey/site-template/ "trey's site-template &mdash; GitHub")
--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
@@ -7,3 +8,14 @@ urlpatterns = patterns('',
     # Uncomment this for admin:
     (r'^admin/', include('django.contrib.admin.urls')),
 )
+
+# This allows Django to serve static files when you're developing your project.
+# You'll have to do something else in production.
+# More information: http://www.djangoproject.com/documentation/static_files/
+# -----------------------------------------------------------------------------
+if settings.DEBUG:
+    urlpatterns += patterns("django.views",
+        url(r"^static/(?P<path>.*)", "static.serve", {
+            "document_root": settings.MEDIA_ROOT,
+        })
+    )
