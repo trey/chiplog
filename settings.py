4175e3db56f6241731a6e60be66390389f4c695d
Trey Piepmeier <trey@sitening.com>
2008-07-13 13:26:11 -0500

    Removing db from repo, starting to customize the admin interface for some reason.

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/Users/trey/Sites/Personal/Django/logbook-django/static/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -70,6 +70,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/trey/Sites/Personal/Django/logbook-django/templates'
 )
 
 INSTALLED_APPS = (
