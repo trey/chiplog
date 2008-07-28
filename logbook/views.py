63d7198ac0a555771d20f6c76564c33f2a6b0a52
Trey Piepmeier <trey@treypiepmeier.com>
2008-07-27 20:14:55 -0500

    Rocking a real form, son!  Watch that indentation

--- logbook/views.py
+++ logbook/views.py
@@ -3,14 +3,15 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from logbook.forms import LogForm
+from logbook.models import Entry
 
 def add_entry(request):
+    entry = Entry()
     if request.POST == 'POST':
-        entry = Entry()
         form = LogForm(data=request.POST, instance=entry)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/')
-        else:
-            form = LogForm(instance=entry)
-        return render_to_response('log_home.html', {'form': form})
+    else:
+        form = LogForm(instance=entry)
+    return render_to_response('log_home.html', {'form': form})
