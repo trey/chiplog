7f4117f78a87a96c2922d3bf0626c6398dd394d0
Trey Piepmeier <trey@treypiepmeier.com>
2008-08-02 19:32:43 -0500

    Renamed Chiplog media/ folder to static/, changed folder settings.

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/Users/trey/Sites/Personal/Django/chiplog_proj/static/'
+MEDIA_ROOT = '/Users/trey/Sites/Personal/Django/chiplog_proj/public/static/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -45,7 +45,7 @@ MEDIA_URL = '/static/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '4zjx(=1_b9f8j0f0!5^s*glw5r1*8e*rx&#7dak_vrf6#(1$k2'
