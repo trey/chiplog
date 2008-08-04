7386076b332d020d149d01bb1c1b3a831cb74022
Trey Piepmeier <trey@treypiepmeier.com>
2008-08-04 09:44:06 -0500

    Added JS to focus on textarea on page load.

--- chiplog/static/js/base.js
+++ chiplog/static/js/base.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+    // Focus on the textarea right away.  You're here to write.
+    $('#id_body').focus();
+
     if($('#s').val() == "") {
 		$('#s').toggleVal({ populateFrom: 'label', removeLabels: true });
 	} else {
