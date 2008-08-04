897aa046749cde6fbe9d495b9edda95290b1758c
Trey Piepmeier <trey@treypiepmeier.com>
2008-08-04 12:14:25 -0500

    Formatted Entry form a little differently, added JS to hide the Tags field with a link to show it.

--- chiplog/static/js/base.js
+++ chiplog/static/js/base.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
     // Focus on the textarea right away.  You're here to write.
     $('#id_body').focus();
 
+    // Only show the Tags field if you really want it.
+    $('#id_tags').hide();
+    $('.tags').append('<a href="#">Add Tags</a>');
+    $('.tags a').click(function() {
+        $('#id_tags').fadeIn();
+        $('#id_tags').focus();
+        $(this).hide();
+    });
+    
+
     if($('#s').val() == "") {
 		$('#s').toggleVal({ populateFrom: 'label', removeLabels: true });
 	} else {
