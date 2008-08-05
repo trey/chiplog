62941e551558cd1f2280a245f04328052a1ade75
Trey Piepmeier <trey@treypiepmeier.com>
2008-08-04 21:26:38 -0500

    JS whitespace.

--- chiplog/static/js/base.js
+++ chiplog/static/js/base.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
-    // Focus on the textarea right away.  You're here to write.
-    $('#id_body').focus();
+	// Focus on the textarea right away.  You're here to write.
+	$('#id_body').focus();
 
-    // Only show the Tags field if you really want it.
-    $('#id_tags').hide();
-    $('.tags').append('<a href="#">Add Tags</a>');
-    $('.tags a').click(function() {
-        $('#id_tags').fadeIn();
-        $('#id_tags').focus();
-        $(this).hide();
-        return false;
-    });
-    
+	// Only show the Tags field if you really want it.
+	$('#id_tags').hide();
+	$('.tags').append('<a href="#">Add Tags</a>');
+	$('.tags a').click(function() {
+		$('#id_tags').fadeIn();
+		$('#id_tags').focus();
+		$(this).hide();
+		return false;
+	});
 
-    if($('#s').val() == "") {
+	// Search form default text and reset on focus.
+	if($('#s').val() == "") {
 		$('#s').toggleVal({ populateFrom: 'label', removeLabels: true });
 	} else {
 		$('#header div label').hide();
