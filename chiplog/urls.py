67fc687291f37fe7aef5de03a15b420241166b50
Trey Piepmeier <trey@treypiepmeier.com>
2008-09-09 00:15:39 -0500

    Used basic.blog as a reference to split out view info out of urls.py.

--- chiplog/urls.py
+++ chiplog/urls.py
@@ -1,32 +1,24 @@
 from django.conf.urls.defaults import *
-from django.views.generic import create_update
-from django.views.generic import date_based
-from django.views.generic import list_detail
-from chiplog.models import Entry
 from chiplog import views
 
-chiplog_list = {
-    'queryset': Entry.objects.all(),
-    'template_object_name': 'entry',
-    'template_name': 'read.html',
-    'paginate_by': 10,
-}
-chiplog_delete = {
-    'model': Entry,
-    'template_name': 'delete.html',
-    'post_delete_redirect': '/chiplog/',
-}
-chiplog_save = {
-    'model': Entry,
-    'template_name': 'create_update.html',
-    'extra_context': { 'entry_list': Entry.objects.all },
-    'post_save_redirect': '/chiplog/',
-}
-
 urlpatterns = patterns('',
-    (r'^$', list_detail.object_list, chiplog_list),
-    (r'^page/(?P<page>\w)/$', list_detail.object_list, chiplog_list),
-    (r'^new/$', create_update.create_object, chiplog_save),
-    (r'^edit/(?P<object_id>\d+)/$', create_update.update_object, chiplog_save),
-    (r'^delete/(?P<object_id>\d+)/$', create_update.delete_object, chiplog_delete)
+    url(r'^$',
+        view=views.entry_list,
+        name='chiplog_index'),
+
+    url(r'^page/(?P<page>\w)/$',
+        view=views.entry_list,
+        name='chiplog_index_paginated'),
+
+    url(r'^new/$',
+        view=views.entry_create,
+        name='chiplog_create'),
+
+    url(r'^edit/(?P<object_id>\d+)/$',
+        view=views.entry_update,
+        name='chiplog_update'),
+
+    url(r'^delete/(?P<object_id>\d+)/$',
+        view=views.entry_delete,
+        name='chiplog_delete'),
 )
--- chiplog/views.py
+++ chiplog/views.py
@@ -1,5 +1,39 @@
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
+from django.views.generic import date_based, list_detail, create_update
+from chiplog.models import *
 
-#
+def entry_list(request, page=0):
+    return list_detail.object_list(
+        request,
+        queryset = Entry.objects.all(),
+        template_object_name = 'entry',
+        template_name = 'read.html',
+        paginate_by = 10,
+    )
+
+def entry_delete(request):
+    return create_update.delete_object(
+        request,
+        model = Entry,
+        template_name = 'delete.html',
+        post_delete_redirect = '/chiplog/',
+    )
+
+def entry_create(request):
+    return create_update.create_object(
+        request,
+        model = Entry,
+        template_name = 'create_update.html',
+        post_save_redirect = '/chiplog/',
+    )
+
+def entry_update(request):
+    return create_update.update_object(
+        request,
+        model = Entry,
+        template_name = 'create_update.html',
+        post_save_redirect = '/chiplog/',
+    )
+
