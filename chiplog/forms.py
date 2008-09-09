d67a52979d54f5a4331669295ad3a379a904567a
Trey Piepmeier <trey@treypiepmeier.com>
2008-09-09 00:53:05 -0500

    Multiple things happening in a single view

--- chiplog/forms.py
+++ chiplog/forms.py
@@ -1,4 +1,6 @@
-from models import Entry
 from django import forms
+from models import Entry
 
-#
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model=Entry
--- chiplog/views.py
+++ chiplog/views.py
@@ -3,15 +3,23 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.views.generic import date_based, list_detail, create_update
 from chiplog.models import *
+from chiplog.forms import EntryForm
 
 def entry_list(request, page=0):
-    return list_detail.object_list(
-        request,
-        queryset = Entry.objects.all(),
-        template_object_name = 'entry',
-        template_name = 'read.html',
-        paginate_by = 10,
-    )
+    if request.POST:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/chiplog/')
+    else:
+        return list_detail.object_list(
+            request,
+            queryset = Entry.objects.all(),
+            template_object_name = 'entry',
+            template_name = 'read.html',
+            paginate_by = 10,
+            extra_context = {'form': EntryForm}
+        )
 
 def entry_delete(request):
     return create_update.delete_object(
