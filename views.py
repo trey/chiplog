800092811e5599ab73a3e1875f36a1a5e82aae18
Trey Piepmeier <trey@treypiepmeier.com>
2008-11-25 16:34:57 -0600

    Working on tagged view.

--- views.py
+++ views.py
@@ -11,87 +11,93 @@ from chiplog import settings
 
 @permission_required('entries.can_add')
 def entry_list(request, page=0):
-	if request.POST:
-		form = EntryForm(data=request.POST)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect(reverse('chiplog_index'))
-		else:
-			return render_to_response('entry_form.html', {'form': form, 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL})
-	else:
-		return list_detail.object_list(
-			request,
-			queryset = Entry.objects.all(),
-			template_object_name = 'entry',
-			template_name = 'entry_list.html',
-			paginate_by = 10,
-			page = page,
-			extra_context = { 'form': EntryForm, 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
-		)
+    if request.POST:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('chiplog_index'))
+        else:
+            return render_to_response('entry_form.html', {'form': form, 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL})
+    else:
+        return list_detail.object_list(
+            request,
+            queryset = Entry.objects.all(),
+            template_object_name = 'entry',
+            template_name = 'entry_list.html',
+            paginate_by = 10,
+            page = page,
+            extra_context = { 'form': EntryForm, 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
+        )
 
 @permission_required('entries.can_add')
 def tagged_list(request, tag):
     """
     All entries for a particular tag.
     """
-    pass
+    return list_detail.object_list(
+        request,
+        queryset = Entry.objects.filter(tags__icontains=tag),
+        template_name = 'entry_list.html',
+        template_object_name = 'entry',
+        extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
+    )
 
 @permission_required('entries.can_add')
 def entry_detail(request, object_id):
-	return list_detail.object_detail(
-		request,
-		object_id = object_id,
-		queryset = Entry.objects.all(),
-		template_name = 'entry_detail.html',
-		template_object_name = 'entry',
-		extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
-	)
+    return list_detail.object_detail(
+        request,
+        object_id = object_id,
+        queryset = Entry.objects.all(),
+        template_name = 'entry_detail.html',
+        template_object_name = 'entry',
+        extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
+    )
 
 @permission_required('entries.can_delete')
 def entry_delete(request, object_id):
-	return create_update.delete_object(
-		request,
-		object_id = object_id,
-		model = Entry,
-		template_name = 'entry_confirm_delete.html',
-		post_delete_redirect = reverse('chiplog_index'),
-		template_object_name = 'entry',
-		extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL, 'referrer': request.META['HTTP_REFERER'] }
-	)
+    return create_update.delete_object(
+        request,
+        object_id = object_id,
+        model = Entry,
+        template_name = 'entry_confirm_delete.html',
+        post_delete_redirect = reverse('chiplog_index'),
+        template_object_name = 'entry',
+        extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL, 'referrer': request.META['HTTP_REFERER'] }
+    )
 
 @permission_required('entries.can_add')
 def entry_create(request):
-	return create_update.create_object(
-		request,
-		model = Entry,
-		template_name = 'entry_form.html',
-		post_save_redirect = reverse('chiplog_index'),
-		extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
-	)
+    return create_update.create_object(
+        request,
+        model = Entry,
+        template_name = 'entry_form.html',
+        post_save_redirect = reverse('chiplog_index'),
+        extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
+    )
 
 @permission_required('entries.can_update')
 def entry_update(request, object_id):
-	return create_update.update_object(
-		request,
-		object_id = object_id,
-		model = Entry,
-		template_name = 'entry_form.html',
-		# post_save_redirect = request.POST['referrer'],
-		template_object_name = 'entry',
-		extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL, 'referrer': request.META['HTTP_REFERER'] }
-	)
+    return create_update.update_object(
+        request,
+        object_id = object_id,
+        model = Entry,
+        template_name = 'entry_form.html',
+        # post_save_redirect = request.POST['referrer'],
+        template_object_name = 'entry',
+        extra_context = { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL, 'referrer': request.META['HTTP_REFERER'] }
+    )
 
 @permission_required('entries.can_add')
 def search(request):
-	if request.GET:
-		search_term = '%s' % request.GET['s']
-		if len(search_term) != 0:
-			entry_list = Entry.objects.filter(body__icontains=search_term)
-			context = { 'entry_list': entry_list, 'search_term':search_term, 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
-			return render_to_response('entry_search.html', context, context_instance=RequestContext(request))
-		else:
-			message = 'Search term was too vague. Please try again.'
-			context = { 'message':message, 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
-			return render_to_response('entry_search.html', context, context_instance=RequestContext(request))
-	else:
-		return render_to_response('entry_search.html', { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }, context_instance=RequestContext(request))
+    if request.GET:
+        search_term = '%s' % request.GET['s']
+        if len(search_term) != 0:
+            entry_list = Entry.objects.filter(body__icontains=search_term)
+            context = { 'entry_list': entry_list, 'search_term':search_term, 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
+            return render_to_response('entry_search.html', context, context_instance=RequestContext(request))
+        else:
+            message = 'Search term was too vague. Please try again.'
+            context = { 'message':message, 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }
+            return render_to_response('entry_search.html', context, context_instance=RequestContext(request))
+    else:
+        return render_to_response('entry_search.html', { 'chiplog_media_url': settings.CHIPLOG_MEDIA_URL }, context_instance=RequestContext(request))
